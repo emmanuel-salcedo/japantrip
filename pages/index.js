b35978a492b6262a15d3e99eefc612620562e5b3
emmanuel-salcedo <mail@emmanuelsalcedo.com>
2024-06-03 11:55:00 -0700

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import Layout from '../components/Layout'
+import BlogList from '../components/BlogList'
+
+const Index = props => {
+  return (
+    <Layout
+      pathname="/"
+      siteTitle={props.title}
+      siteDescription={props.description}
+    >
+      <section>
+        <BlogList/>
+      </section>
+    </Layout>
+  )
+}
+
+export default Index
+
+export async function getStaticProps() {
+  const siteConfig = await import(`../data/config.json`)
+
+  return {
+    props: {
+      title: siteConfig.default.title,
+      description: siteConfig.default.description,
+    },
+  }
+}
